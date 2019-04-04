dfcc8576b5a67c01ebb249c655732e9360ea4ee8
Héctor Torres <41318456+HectorTorresQP@users.noreply.github.com>
2019-04-04 13:44:01 -0700

    Scriptable object for generate extinguisher object
    First commit.

---
+++ Extinguisher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Extinguisher", menuName = "Extintor QP")]
+public class Extinguisher : ScriptableObject
+{
+    public Material colorExt;
+
+    public GameObject modelExtinguisher;
+    private GameObject modelBoquilla;
+    private LineRenderer lineHose;
+    private int damageFire;
+
+    public void ChangeMaterial()
+    {
+        modelExtinguisher.GetComponent<Renderer>().material = colorExt;
+    }
+
+    public void Apagar()
+    {
+        Debug.Log("Apagado");
+    }
+    public void Residuos()
+    {
+        Debug.Log("Residuos");
+    }
+}
